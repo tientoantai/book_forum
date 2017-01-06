498f8a3f8fbf9b2d310ab5ea21c1fca7fd5f34aa
thaodoremon <thuthaonguyen1509@gmail.com>
2017-01-06 15:54:29 +0700

    Book_forum

---
+++ app/BookForum/Book/Book.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thuthao
+ * Date: 06/01/2017
+ * Time: 15:52
+ */
+
+namespace App\BookForum\Book;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+
+}
