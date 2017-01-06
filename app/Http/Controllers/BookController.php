73de1e0afe0d3625b6fdeef686e12347b0c3d366
thaodoremon <thuthaonguyen1509@gmail.com>
2017-01-06 16:46:22 +0700

    Book_forum

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,5 +6,49 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    public function listBook(){
+        return view('book')->with('booklist', Book::all());
+    }
+    public function delete($id)
+    {
+        $book = Book::where('id', '=', $id)->delete();
+        return redirect('/books')
+    }
+    public function detail($id)
+    {
+        $book = Book::where('id', '=', $id);
+        return view('detailbook')->with('book', $book);
+    }
+    public function insertform()
+    {
+        return view('insert');
+    }
+    public  function insert()
+    {
+        $book = new Book;
+        $book->author = request('author');
+        $book->price = request('price');
+        $book->publisher = request('publisher');
+        $book->title = request('title');
+        $book->image = request('image');
+        $book->save();
+        return redirect('/books');
 
+    }
+    public  function updateform($id)
+    {
+        $book = Book:where('id', '=', $id);
+        return view('updatebook')->with('book', $book);
+    }
+    public function update($id)
+    {
+        $book = Book::where('id', '=', $id);
+        $book->author = request('author');
+        $book->price = request('price');
+        $book->publisher = request('publisher');
+        $book->title = request('title');
+        $book->image = request('image');
+        $book->save();
+        return redirect('/books');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/books', 'BookController@listBook');
+Route::get('/delete/{id}', 'BookController@delete');
+Route::get('/detail/{id}', 'BookController@detail');
+Route::get('/insert', 'BookController@insertform');
+Route::post('/insert', 'BookController@insert');
+Route::get('/update/{id}', 'BookController@updateform');
+Route::get('/update/{id}', 'BookController@update');
