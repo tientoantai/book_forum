2785cdbb1df749dd25e0daec8a227137b7314368
lawliet <daont@sphinx-sofware.com>
2017-02-09 09:52:00 +0700

    authentication revire

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,31 +22,31 @@ class LoginController extends Controller
 
 	public function authenticate(Request $request)
 	{
-        $username   = $request->get('username');
-        $password   = $request->get('password');
-        $credential = $this->authenticator->authenticate($username, $password);
+                $username   = $request->get('username');
+                $password   = $request->get('password');
+                $credential = $this->authenticator->authenticate($username, $password);
 
-        if($credential instanceof LoginFailMessage)
-        {
-        	return view('books.login')
-        			->with([
-        				'messages' => $credential->toString(),
-        				'username' => $username,
-        			])
-        	;
-        }
-        else
-        {
-        	request()->session()->put('credential', $credential);
+                if($credential instanceof LoginFailMessage)
+                {
+                	return view('books.login')
+                		->with([
+                			'messages' => $credential->toString(),
+                			'username' => $username,
+                			])
+                	;
+                }
+                else
+                {
+                        request()->session()->put('credential', $credential);
 
-        	return response()->redirectToRoute('books.admin');
-        }
+                	return response()->redirectToRoute('books.admin');
+                }
 	}
 
 	public function logout(Request $request)
 	{
 		$request->session()->forget('credential');
             
-        return response()->redirectToRoute('login.index');
+                return response()->redirectToRoute('login.index');
 	}
 }
